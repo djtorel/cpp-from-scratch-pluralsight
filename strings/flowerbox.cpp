40a4d918898fef596dbed45af2be8d913cfb07c9
Dominic Torruellas <dtorruellas@gmail.com>
2017-07-29 16:00:49 -0700

    flowerbox example

---
+++ strings/flowerbox.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  cout << "Message? ";
+  string message{};
+  getline(cin, message);
+
+  string flowerLine(message.length() + 4, '*');
+
+  cout << '\n';
+
+  cout << flowerLine << '\n';
+  cout << "* " << message << " *\n";
+  cout << flowerLine << '\n';
+}
