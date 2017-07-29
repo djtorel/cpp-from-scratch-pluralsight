b76f7f32e2443497abdf9765e75afc913cef5627
Dominic Torruellas <dtorruellas@gmail.com>
2017-07-29 15:41:11 -0700

    strings example 1

---
+++ strings/greetings.cpp
@@ -0,0 +1,13 @@
+#include <iostream> // for std::cin, and std::cout
+#include <string> // For std::string
+using namespace std;
+
+int main() {
+  cout << "What's your name? ";
+  string name{};
+  cin >> name;
+
+  cout << '\n';
+  cout << "Hi, " << name << "!\n";
+  cout << "Nice to meet you! :-)\n";
+}
